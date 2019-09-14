05c8d5339a1dadc4a5ed7d3e606e68be609b6c7f
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-09-14 16:39:03 -0700

    create form helpers, able to create new article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
-class Article < ActiveRecord::Base
-    validates :title, presence: true, length: { minimun: 3, maximun: 20}
-    validates :description, presence: true, length: { minimun: 3, maximun: 50} 
+class Article < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 3, maximum: 10}
+    validates :description, presence: true, length: {minimum: 3, maximum: 30} 
 end
