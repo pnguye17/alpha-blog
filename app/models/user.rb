72ed0f555c9b45905f58b1b88b9e2a303f681f58
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-09-17 16:11:30 -0700

    validates users info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { minimum: 3}
-    validates :email, presence: true, length: { minimum: 3 } 
+    VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :username, presence: true, uniquness: { case_sensitive: false }, length: { minimum: 3}
+    validates :email, presence: true, uniquness: { case_sensitive: false }, length: { minimum: 3 }, format: { with: VALID_EMAIL} 
 end
