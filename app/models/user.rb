4a6b234d7f1253bdae781eacba9a5f14da6e37ec
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-09-17 16:58:29 -0700

    add gem 'bcrypt', '~> 3.1', '>= 3.1.12'

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     before_save { self.email = email.downcase }
     validates :username, presence: true, uniquness: { case_sensitive: false }, length: { minimum: 3}
     VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, uniquness: { case_sensitive: false }, length: { minimum: 3 }, format: { with: VALID_EMAIL} 
+    validates :email, presence: true, uniquness: { case_sensitive: false }, length: { minimum: 3 }, format: { with: VALID_EMAIL}
+    has_secure_password 
 end
