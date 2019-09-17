ce689df506d4971bc1e80d0ae18d0b1e76c2d803
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-09-17 16:18:54 -0700

    update and deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :username, presence: true, uniquness: { case_sensitive: false }, length: { minimum: 3}
+    VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniquness: { case_sensitive: false }, length: { minimum: 3 }, format: { with: VALID_EMAIL} 
 end
