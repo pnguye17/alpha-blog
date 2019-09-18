edb754d5b57d5bba97e698a3813f6c7abf7e44d6
Peter Nguyen <peternguyen@Peters-MacBook-Air.local>
2019-09-17 17:35:21 -0700

    UsersController#new template isnt working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_many :articles
     before_save { self.email = email.downcase }
-    validates :username, presence: true, uniquness: { case_sensitive: false }, length: { minimum: 3}
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3}
     VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, uniquness: { case_sensitive: false }, length: { minimum: 3 }, format: { with: VALID_EMAIL}
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3 }, format: { with: VALID_EMAIL}
     has_secure_password 
 end
